69d205c48b80c878b734faa74094870ebac347ae
Kawaiiow <poohsn1357@gmail.com>
2024-07-13 01:31:52 +0700

    Bake DONUT

---
+++ lab_02/donut.py
@@ -0,0 +1,24 @@
+"""Blender Guru - Blender Tutorial for Complete Beginners \
+    https://www.youtube.com/watch?v=B0J27sf9N1Y&list=PLjEaoINr3zgEPv5y--4MKpciLaoQYZB1Z """
+
+
+def main():
+    """void"""
+    p = int(input())
+    i = int(input())
+    j = int(input())
+    n = int(input())
+    bundle = 0
+    left = 0
+
+    if i <= n <= i + j:
+        print(p * i, i + j)
+        return
+
+    bundle = n // (i + j)
+    left = n - (bundle * (i + j))
+    if left >= i:
+        left = 0
+        bundle += 1
+    print(((p * i) * bundle) + (p * left), ((i + j) * bundle) + (left))
+main()
