8163bfd268fd2a1bd9daf7265350a429615b6c0f
Kawaiiow <poohsn1357@gmail.com>
2024-07-13 00:12:20 +0700

    Validated all cases

--- lab_02/triangle_I.py
+++ lab_02/triangle_I.py
@@ -1,14 +1,35 @@
 """triangle"""
 
+
+def n_cmp(n1, n2):
+    """cmp"""
+    if n1 > n2:
+        return 1
+    return 0
+
+
 def main():
     """45"""
     a = float(input())
     b = float(input())
     c = float(input())
 
+    if n_cmp(a, b):
+        a = a + b
+        b = a - b
+        a = a - b
+    if n_cmp(b, c):
+        b = c + b
+        c = b - c
+        b = b - c
+    if n_cmp(a, b):
+        a = a + b
+        b = a - b
+        a = a - b
+
     if a <= 0 or b <= 0 or c <= 0:
         print("No")
-    elif 0 <= c**2 - (a**2 + b**2) <= 0.01:
+    elif 0 <= abs(c**2 - (a**2 + b**2)) <= 0.01:
         print("Yes")
     else:
         print("No")
