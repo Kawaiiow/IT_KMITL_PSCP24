550dd7251c198c571971e1d32034f25e80c46fa6
Kawaiiow <poohsn1357@gmail.com>
2024-07-23 09:17:21 +0700

    correct all cases

--- lab_03/grade_III.py
+++ lab_03/grade_III.py
@@ -1,35 +1,35 @@
 """Grade A"""
-import math
+
+
+def point_cmp(n):
+    """float"""
+    point = 0
+    if 95.00 <= n <= 100.00:
+        point = 4.00
+    elif 90.00 <= n < 95.00:
+        point = 3.50
+    elif 85.00 <= n < 90.00:
+        point = 3.00
+    elif 80.00 <= n < 85.00:
+        point = 2.50
+    elif 75.00 <= n < 80.00:
+        point = 2.00
+    elif 70.00 <= n < 75.00:
+        point = 1.50
+    elif 65.00 <= n < 70.00:
+        point = 1.00
+    elif 60.00 <= n < 65.00:
+        point = 0.50
+    else:
+        point = 0.00
+    return point
+
 
 def main():
     """void"""
     c = int(input())
-    sums_point = 0
+    sums_point = 0.00
     for _ in range(c):
-        sums_point += float(input()[:-1])
-
-    sums_point /= c
-
-    if 95 <= sums_point <= 100:
-        print("4.00")
-    if 90 <= sums_point < 95:
-        print("3.50")
-    if 85 <= sums_point < 90:
-        print("3.00")
-    if 80 <= sums_point < 85:
-        print("2.50")
-    if 75 <= sums_point < 80:
-        print("2.00")
-    if 70 <= sums_point < 75:
-        print("1.50")
-    if 65 <= sums_point < 70:
-        print("1.00")
-    if 60 <= sums_point < 65:
-        print("0.50")
-    else:
-        print("0.00")
-# main()
-
-a = 10.417123124
-a = float(str(a)[:-1])
-print(f"{a}")
+        sums_point += point_cmp(float(input()))
+    print(f"{(int(sums_point / c * 100) / 100):.2f}")
+main()
