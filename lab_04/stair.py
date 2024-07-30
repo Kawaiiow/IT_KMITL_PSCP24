9d208eb12ef3fabf1c85b606189a878ca873ad52
Kawaiiow <poohsn1357@gmail.com>
2024-07-30 11:32:20 +0700

    update

--- lab_04/stair.py
+++ lab_04/stair.py
@@ -1,23 +1,27 @@
 """Up to the heaven"""
-import math
 
 def main(h, n):
     """void"""
     sums = 0
+    tmp = 0
     step = 0
     state = 1
 
     for _ in range(n):
         step = int(input())
+        tmp += step
         if step > h:
             state = 0
-        sums += step
-    if not h and not sums:
-        print("1")
-        return
+        if tmp == h:
+            tmp = 0
+            sums += 1
+        elif tmp > h:
+            sums += 1
+            tmp = step
+    if tmp > 0:
+        sums += 1
     if not state:
         print("NO")
     else:
-        print(f"{math.ceil(sums/h):.0f}")
+        print(sums)
 main(int(input()),int(input()))
-
