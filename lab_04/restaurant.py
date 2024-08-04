7389844f518844befdfc3bfd9905dc5b2bbc2429
Kawaiiow <poohsn1357@gmail.com>
2024-08-04 22:46:53 +0700

    Recorrect

--- lab_04/restaurant.py
+++ lab_04/restaurant.py
@@ -8,15 +8,19 @@ def main():
     add = float(input())
     sums = current + add
     discount = 0
+    after = 0
 
+    if current >= trig:
+        current -= current * (percent / 100)
     if sums >= trig:
         discount = sums * (percent/100)
-        if discount == add:
+        after = sums - discount
+        if after == current:
             print("Yes")
-        elif discount > add:
-            print(f"Yes {abs(sums - (sums - discount)):.3f}")
-        elif discount < add:
-            print(f"No {abs(sums - (sums - discount)):.3f}")
+        elif current > after:
+            print(f"Yes {abs(current - (sums - discount)):.3f}")
+        elif current < after:
+            print(f"No {abs(current - (sums - discount)):.3f}")
     else:
         print("Yes")
 main()
