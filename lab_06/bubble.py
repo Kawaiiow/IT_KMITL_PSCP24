4526f1a3d60949cee4cd99073b350cd3ee897643
Kawaiiow <poohsn1357@gmail.com>
2024-08-23 09:55:19 +0700

    Add Prototype Bubble

---
+++ lab_06/bubble.py
@@ -0,0 +1,24 @@
+"""Buuble Sort isn't way to go to"""
+
+def check_path(msg):
+    """void"""
+
+def small_bubble(msg):
+    """int"""
+    if msg in ("")
+    return 
+
+def big_bubble(msg):
+    """void"""
+
+def main():
+    """void"""
+    msg = input()
+    pos = 0
+    move_state = 1
+    finish = 0
+
+    while move_state and not finish:
+        if msg[pos] == "o":
+            if msg[pos + 1] 
+
