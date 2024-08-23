036132d8832fb88e8ec1949080b7207fd3cbc4ae
Kawaiiow <poohsn1357@gmail.com>
2024-08-23 10:57:39 +0700

    edit ubble

--- lab_06/bubble.py
+++ lab_06/bubble.py
@@ -2,14 +2,42 @@
 
 def check_path(msg):
     """void"""
+    move_pos = 0
+
+    if msg[0] == 'o':
+        move_pos += small_bubble(msg[1])
+    elif msg[0] == "O":
+        # print("Enter big")
+        # print(msg[1:4])
+        move_pos += big_bubble(msg[1:4])
+    return move_pos
 
 def small_bubble(msg):
     """int"""
-    if msg in ("")
-    return 
+    if not msg in ('o', 'O'):
+        return 0
+    return 1
 
 def big_bubble(msg):
     """void"""
+    pos = 0
+    best = 0
+    back = 3
+
+    print(msg)
+    if msg == "   ":
+        return 0
+    for i in range(len(msg) - 1 , -1, -1):
+        # print(msg[i])
+        if msg[i] == " ":
+            continue
+        elif msg[i] == "o" and i > pos and not best:
+            pos = i
+        elif msg [i] == "O" and not best:
+            pos = i
+            best = 1
+    # print(pos)
+    return pos + 1
 
 def main():
     """void"""
@@ -19,6 +47,11 @@ def main():
     finish = 0
 
     while move_state and not finish:
-        if msg[pos] == "o":
-            if msg[pos + 1] 
+        check_path(msg[pos:])
 
+def test():
+    """void"""
+    a = "oOO"
+    while 
+    print(pos, a[pos])
+test()
