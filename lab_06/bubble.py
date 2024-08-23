4f83220343f627d023455710804356c4aa88c5f8
Kawaiiow <poohsn1357@gmail.com>
2024-08-24 00:14:00 +0700

    Finalize Bubble

--- lab_06/bubble.py
+++ lab_06/bubble.py
@@ -4,17 +4,16 @@ def check_path(msg):
     """void"""
     move_pos = 0
 
-    if msg[0] == 'o':
+    if msg[0] in ('^', 'o'):
         move_pos += small_bubble(msg[1])
     elif msg[0] == "O":
-        # print("Enter big")
-        # print(msg[1:4])
         move_pos += big_bubble(msg[1:4])
+    # print(msg)
     return move_pos
 
 def small_bubble(msg):
     """int"""
-    if not msg in ('o', 'O'):
+    if not msg in ('o', 'O', '|', '^'):
         return 0
     return 1
 
@@ -22,36 +21,37 @@ def big_bubble(msg):
     """void"""
     pos = 0
     best = 0
-    back = 3
 
-    print(msg)
     if msg == "   ":
         return 0
     for i in range(len(msg) - 1 , -1, -1):
-        # print(msg[i])
-        if msg[i] == " ":
-            continue
-        elif msg[i] == "o" and i > pos and not best:
+        if msg[i] == "|" and not best:
             pos = i
+            best = 1
         elif msg [i] == "O" and not best:
             pos = i
             best = 1
-    # print(pos)
+        elif msg[i] == "o" and i > pos and not best:
+            pos = i
     return pos + 1
 
 def main():
     """void"""
     msg = input()
+    i = 0
     pos = 0
+    temp = 0
     move_state = 1
     finish = 0
 
     while move_state and not finish:
-        check_path(msg[pos:])
-
-def test():
-    """void"""
-    a = "oOO"
-    while 
-    print(pos, a[pos])
-test()
+        temp = pos
+        pos += check_path(msg[pos:])
+        if temp == pos:
+            move_state = 0
+        if msg[pos] == '|':
+            finish = 1
+        i += 1
+    print("POSSIBLE" if finish else "IMPOSSIBLE")
+    print(i if finish else len(msg) - (pos + 1))
+main()
